5bb7046e2dbc3070548c2a1469f568c08e1bd35c
Amir Bashan <90416342+amirbashan@users.noreply.github.com>
2022-06-27 11:12:38 +0300

    chaser and runner done

--- game.js
+++ game.js
@@ -10,28 +10,33 @@ const updateInterval = (bool) => {
     points = points + 5;
   } else {
     clearInterval(interval);
+    start.style.display = 'block';
+    start.innerText = `Game Over!\nClick here to try again`;
   }
 };
 const getSpeed = () => Math.ceil(Math.random() * 100);
+
+const chaser = new Pieces(document.getElementById('chaser'), '🔴', false, getSpeed(), updateInterval);
+const blocker = new Pieces(document.getElementById('blocker'), '⚠️', false, getSpeed(), updateInterval);
+const escaper = new Pieces(document.getElementById('escaper'), '🟩', true, getSpeed(), updateInterval);
 
 gameBoard.addEventListener('mousemove', (e) => {
   cursorX = e.clientX;
   cursorY = e.clientY;
 });
 
-const chaser = new Pieces(document.getElementById('chaser'), '🔴', false, getSpeed(), updateInterval);
-const escaper = new Pieces(document.getElementById('escaper'), '🟩', true, getSpeed(), updateInterval);
-
 start.addEventListener('click', () => {
   start.style.display = 'none';
   chaser.startPiece();
   escaper.startPiece();
+  blocker.startPiece();
 
   let scoreCounter = 0;
   interval = setInterval(() => {
     scoreCounter += 1;
     score.innerText = scoreCounter + points;
-    chaser.movement(cursorX, cursorY);
-    escaper.movement(cursorX, cursorY);
+    chaser.movementCursor(cursorX, cursorY);
+    blocker.movementCursor(cursorX, cursorY);
+    escaper.movementCursor(cursorX, cursorY);
   }, 1000);
 });
--- pieces.js
+++ pieces.js
@@ -6,17 +6,6 @@ class Pieces {
     this.speed = speed;
     this.updateInterval = updateInterval;
   }
-  randomX() {
-    // const plusOrMinus = Math.random() < 0.5 ? -1 : 1;
-    console.log(this.element.style.fontSize);
-    const num = Math.random() * parent.innerWidth;
-    this.element.style.left = num + 'px';
-  }
-  randomY() {
-    // const plusOrMinus = Math.random() < 0.5 ? -1 : 1;
-    const num = Math.random() * parent.innerHeight;
-    this.element.style.top = num + 'px';
-  }
 
   startPiece() {
     this.element.innerText = this.shape;
@@ -29,22 +18,25 @@ class Pieces {
     this.element.addEventListener('mouseover', () => this.onHitTarget());
   }
 
-  movement(cursorX, cursorY) {
-    let directionFixY = this.isTypeGood ? this.speed * -1 : this.speed;
-    let shapeFixY = parseInt(this.element.style.top.slice(0, -2));
-    let gapY = cursorY - shapeFixY;
-    if (gapY < 0) directionFixY = directionFixY * -1;
-    let targetY = this.checkBoundary(shapeFixY + directionFixY, parent.innerHeight);
-    this.element.style.top = targetY + 'px';
-
-    let directionFixX = this.isTypeGood ? this.speed * -1 : this.speed;
-    let shapeFixX = parseInt(this.element.style.left.slice(0, -2));
-    let gapX = cursorX - shapeFixX;
-    console.log('gapX', gapX);
-    if (gapX < 0) directionFixX = directionFixX * -1;
-    let targetX = this.checkBoundary(shapeFixX + directionFixX, parent.innerWidth);
-    this.element.style.left = targetX + 'px';
+  movementCursor(cursorX, cursorY) {
+    this.moveByAxis('X', cursorX);
+    this.moveByAxis('Y', cursorY);
   }
+
+  moveByAxis(axis, cursor) {
+    let towardsOrAway = this.isTypeGood ? this.speed * -1 : this.speed;
+    const orientation = axis === 'Y' ? 'top' : 'left';
+    const direction = axis === 'Y' ? 'innerHeight' : 'innerWidth';
+    const shapeLocation = parseInt(this.element.style[orientation].slice(0, -2));
+    const gap = cursor - shapeLocation;
+    if (gap < 0) towardsOrAway = towardsOrAway * -1;
+    if (Math.abs(gap) < this.speed) towardsOrAway = gap;
+    const target = this.checkBoundary(shapeLocation + towardsOrAway, parent[direction]);
+    this.element.style[orientation] = target + 'px';
+  }
+
+  movementRandom() {}
+
   onHitTarget() {
     updateInterval(this.isTypeGood);
     if (this.isTypeGood) {
@@ -52,10 +44,20 @@ class Pieces {
       this.randomX();
     }
   }
+
+  randomX() {
+    const randomNum = Math.random() * parent.innerWidth;
+    this.element.style.left = this.checkBoundary(randomNum, parent.innerWidth) + 'px';
+  }
+  randomY() {
+    const randomNum = Math.random() * parent.innerHeight;
+    this.element.style.top = this.checkBoundary(randomNum, parent.innerHeight) + 'px';
+  }
+
   checkBoundary(num, max) {
     const shapeSize = parseInt(this.element.style.fontSize.slice(0, -2));
     if (num <= 0 + shapeSize / 2) return 0 + shapeSize / 2;
-    if (num + this.speed + shapeSize / 2 >= max) return max - shapeSize;
+    if (num >= max - shapeSize) return max - shapeSize;
     return num;
   }
 }
